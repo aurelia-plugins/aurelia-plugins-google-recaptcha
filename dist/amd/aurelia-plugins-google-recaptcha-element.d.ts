3679c0cf8772d05a62e3779d5e96934e07120f30
Bart Lenaerts <bart.lenaerts@philips.com>
2017-06-29 11:47:24 +0200

    Typescript definitions

---
+++ dist/amd/aurelia-plugins-google-recaptcha-element.d.ts
@@ -0,0 +1,14 @@
+export declare class Recaptcha {
+    _config: any;
+    _element: any;
+    _scriptPromise: any;
+    callback: any;
+    size: string;
+    theme: string;
+    type: string;
+    widgetId: any;
+    constructor(element: any, config: any);
+    bind(): void;
+    _initialize(): Promise<void>;
+    _loadApiScript(): void;
+}
